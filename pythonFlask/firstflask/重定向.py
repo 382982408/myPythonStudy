910c7a0a7520898c3ba11dc7360bd4ae42a494c0
382982408 <zounsheon@126.com>
2018-03-11 10:10:40 +0800

    redirect

--- pythonFlask/firstflask/重定向.py
+++ pythonFlask/firstflask/重定向.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def index():
     # 第一种重定向方式
     # return redirect('/login/')
-    # 第二种重定向方式，优先考虑
+    # 第二种重定向方式，推荐使用
     return redirect(url_for("login"))
 
     return u'这是首页'
@@ -19,6 +19,15 @@ def index():
 @app.route('/login/')
 def login():
     return u'这是登录页面'
+
+#重定向的应用,判断用户是否已经登录，如果登录成功，则返回发布问答页面
+@app.route('/question/<is_login>/')
+def question(is_login):
+    if is_login == '1':
+        return u'这是问答页面'
+    else:
+        return redirect(url_for("login"))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
